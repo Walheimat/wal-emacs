404f970036e4f9379f8734c6067349f6d6f81ed6
Walheimat <krister.schuchardt@theventury.com>
2020-03-07 14:06:45 +0100

    better setup

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (diff-hl yasnippet yaml-mode web-mode use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil rebecca-theme ov org-bullets nord-theme minimap js2-mode indent-guide flycheck find-file-in-project exec-path-from-shell dracula-theme dimmer company-lsp add-node-modules-path))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-tab ((t (:foreground "#636363")))))
