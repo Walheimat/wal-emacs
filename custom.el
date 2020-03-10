da42dda92a18492bbafc61cf4d2aaa05a0cede90
Walheimat <krister.schuchardt@theventury.com>
2020-03-10 21:38:40 +0100

    add packages, re-organize a bit

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (diff-hl yasnippet yaml-mode web-mode use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil rebecca-theme ov org-bullets nord-theme minimap js2-mode indent-guide flycheck find-file-in-project exec-path-from-shell dracula-theme dimmer company-lsp add-node-modules-path))))
+    (evil-nerd-commenter diff-hl yasnippet yaml-mode web-mode use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil rebecca-theme ov org-bullets nord-theme minimap js2-mode indent-guide flycheck find-file-in-project exec-path-from-shell dracula-theme dimmer company-lsp add-node-modules-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
