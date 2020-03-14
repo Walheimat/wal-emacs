929bce498c5804504d5284a170ff31a76315f875
Krister Schuchardt <krister.schuchardt@theventury.com>
2020-03-14 18:31:59 +0100

    font selection, custom font size arg

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-nerd-commenter diff-hl yasnippet yaml-mode web-mode use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil ov org-bullets js2-mode flycheck find-file-in-project dracula-theme dimmer company-lsp add-node-modules-path))))
+    (angularjs-mode evil-nerd-commenter diff-hl yasnippet yaml-mode web-mode use-package typescript-mode treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil ov org-bullets js2-mode flycheck find-file-in-project dracula-theme dimmer company-lsp add-node-modules-path)))
+ '(zoom-size (quote (0.618 . 0.618))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
