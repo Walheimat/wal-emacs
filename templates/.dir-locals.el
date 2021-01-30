4b84d9e5e4da982f1eb062676bb1bff3d68fa951
Walheimat <krister.schuchardt@theventury.com>
2021-01-30 18:07:04 +0100

    prep v1.2.7

--- templates/.dir-locals.el
+++ templates/.dir-locals.el
@@ -1,9 +1,8 @@
 ;;; dir local variables to work with space indentation
 
-((js2-mode  . ((eval . (local-unset-key (kbd "TAB")))
-	       (indent-tabs-mode . nil)
-	       ;; for example ...
+((js2-mode  . ((wal/prefer-tabs nil)
+	       (js-indent-level . 2)))
+ (rjsx-mode . ((wal/prefer-tabs nil)
 	       (js-indent-level . 2)))
  (scss-mode . ((css-indent-offset . 2)))
  (css-mode  . ((css-indent-offset . 2))))
-
