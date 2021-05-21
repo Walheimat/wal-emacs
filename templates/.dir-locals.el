73562e14fa79ccabbb327e8b139f58d79422d97a
Walheimat <krister.schuchardt@theventury.com>
2021-05-21 22:58:16 +0200

    prep v1.3.7

--- templates/.dir-locals.el
+++ templates/.dir-locals.el
@@ -2,9 +2,5 @@
 
 ((js2-mode  . ((wal/prefer-tabs . nil)
 			   (js-indent-level . 2)))
- (rjsx-mode . ((wal/prefer-tabs . nil)
-			   (js-indent-level . 2)))
- (scss-mode . ((wal/prefer-tabs   . nil)
-			   (css-indent-offset . 2)))
  (css-mode  . ((wal/prefer-tabs   . nil)
 			   (css-indent-offset . 2))))
