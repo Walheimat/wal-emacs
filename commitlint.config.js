d05687cdd122d7a4e836df53c0df0d11388ad8d8
Walheimat <krister.schuchardt@theventury.com>
2021-09-11 15:55:42 +0200

    Improvement: Better commit rules
    Don't constrain type names, just ensure `start-case`.
    
    Add rule for scope.
    
    Ensure leading blank for body.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,17 @@
 module.exports = {
 	rules: {
-		'header-max-length': [2, 'always', 72],
-		'type-enum': [2, 'always', ['Fix', 'Feature', 'Style', 'Refactor', 'Improvement']],
-		'type-case': [2, 'always', ['start-case']],
+		'header-max-length': [2, 'always', 72], // GitHub's limit, GitLab's is 1 KiB
 		'type-empty': [1, 'never'],
-		'subject-case': [2, 'always', ['sentence-case']],
+		'type-case': [2, 'always', ['start-case']],
+        'scope-case': [1, 'always', ['lower-case', 'upper-case']],
 		'subject-empty': [2, 'never'],
+		'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
+        'subject-full-stop': [1, 'never'],
 		'body-empty': [2, 'never'],
+        'body-leading-blank': [1, 'always'],
 		'body-case': [1, 'always', ['lower-case', 'sentence-case']],
 		'body-full-stop': [1, 'always', '.'],
 		'body-max-line-length': [2, 'always', 70],
-		'footer-max-line-length': [2, 'always', 70],
+		'footer-max-line-length': [2, 'always', 70]
 	}
 };
