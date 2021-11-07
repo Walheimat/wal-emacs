b7138e11faec096e7aef8f6df18afd2bf863b5f7
Walheimat <krister.schuchardt@theventury.com>
2021-11-07 13:20:01 +0100

    Scripts: Use two spaces instead of four
    Adapt the scripts to use 2 spaces instead of 4.
    
    This also adds a section for sh-script in the configuration to set
    this and make continuations not align.

--- setup/setup-commits.sh
+++ setup/setup-commits.sh
@@ -3,13 +3,13 @@
 source "./variables.sh"
 
 function install_npm_dependencies() {
-    echo -e "\n${bold}[npm]${reset}"
-    (cd ..; npm install 2>&1)
+  echo -e "\n${bold}[npm]${reset}"
+  (cd ..; npm install 2>&1)
 }
 
 function install_husky() {
-    echo -e "\n${bold}[husky]${reset}\n"
-    (cd ..; npx husky install 2>&1)
+  echo -e "\n${bold}[husky]${reset}\n"
+  (cd ..; npx husky install 2>&1)
 }
 
 echo -e "${blue}${whale}${reset} ${bold}[commit hooks setup]${reset}"
--- setup/setup-init.sh
+++ setup/setup-init.sh
@@ -5,39 +5,39 @@ source "./variables.sh"
 DIR="${0%/*}"
 
 function no_init_file_exists() {
-    local userdir=$1
-    local method=$2
+  local userdir=$1
+  local method=$2
 
-    if [[ -e "$userdir/init.el" || -e "$HOME/.emacs" ]]; then
-        echo -e "${red}${whale}${reset} found existing init file, can't $method"
-        return 1;
-    fi
+  if [[ -e "$userdir/init.el" || -e "$HOME/.emacs" ]]; then
+    echo -e "${red}${whale}${reset} found existing init file, can't $method"
+    return 1;
+  fi
 
-    return 0
+  return 0
 }
 
 function on_complete() {
-    echo -e "\n${green}${whale}${reset} init file setup complete, you can restart Emacs"
+  echo -e "\n${green}${whale}${reset} init file setup complete, you can restart Emacs"
 }
 
 function link_init_file() {
-    local userdir="${1:-$HOME}"
+  local userdir="${1:-$HOME}"
 
-    if no_init_file_exists "$userdir" "link"; then
-        echo -e "\n(symbolically) linking init file to ${userdir@Q}"
-        ln -s "$(cd ..; pwd)/templates/init.el" "$userdir/init.el"
-        on_complete
-    fi
+  if no_init_file_exists "$userdir" "link"; then
+    echo -e "\n(symbolically) linking init file to ${userdir@Q}"
+    ln -s "$(cd ..; pwd)/templates/init.el" "$userdir/init.el"
+    on_complete
+  fi
 }
 
 function copy_init_file() {
-    local userdir="${1:-$HOME}"
+  local userdir="${1:-$HOME}"
 
-    if no_init_file_exists "$userdir" "copy"; then
-        echo -e "\ncopying init file to ${userdir@Q}"
-        cp "$DIR/../templates/init.el" "$userdir/init.el"
-        on_complete
-    fi
+  if no_init_file_exists "$userdir" "copy"; then
+    echo -e "\ncopying init file to ${userdir@Q}"
+    cp "$DIR/../templates/init.el" "$userdir/init.el"
+    on_complete
+  fi
 }
 
 echo -e "${blue}${whale}${reset} ${bold}[init file setup]${reset}"
@@ -45,13 +45,13 @@ echo -e "${blue}${whale}${reset} ${bold}[init file setup]${reset}"
 func=$1
 
 case $func in
-    link)
-        link_init_file "${@:2}"
-        ;;
-    copy)
-        copy_init_file "${@:2}"
-        ;;
-    *)
-        echo -e "${red}${whale}${reset} call with 'link' or 'copy'"
-        ;;
+  link)
+    link_init_file "${@:2}"
+    ;;
+  copy)
+    copy_init_file "${@:2}"
+    ;;
+  *)
+    echo -e "${red}${whale}${reset} call with 'link' or 'copy'"
+    ;;
 esac
