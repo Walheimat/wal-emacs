8e8002577948aa8ad620810d03e439618336348b
Walheimat <krister.schuchardt@gmail.com>
2022-04-05 09:02:10 +0200

    feat(setup): clean-up, daemon script
    Reworks the setup package a bit to be more reliable. Adds new bash
    script to copy the service config using `sed` to get the correct path
    to binary.
    
    Adapts texts to reflect these changes.

---
+++ setup/setup-daemon.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Copy service template and enable the service.
+
+# shellcheck disable=SC2119
+
+source "./variables.sh"
+source "./common.sh"
+
+DIR="${0%/*}"
+
+TARGET_PATH="$HOME/.config/systemd/user/"
+
+DEFAULT_EMACS_PATH="/usr/local/bin/emacs"
+
+function wal::ensure() {
+  if [[ -z "$TARGET_PATH" ]]; then
+    signal "Creating path ${TARGET_PATH@Q}"
+    if ! mkdir -p "$TARGET_PATH" &> "$WAL_LOG"; then
+      die
+    fi
+    live
+  fi
+
+  if [[ -e "$TARGET_PATH/emacs.service" ]]; then
+    echo -e "Found existing service configuration, won't override" >"$WAL_LOG"
+    die
+  fi
+}
+
+function wal::copy_service_template() {
+  wal::ensure
+
+  local emacs_path
+  emacs_path=$(which emacs)
+
+  signal "Copying ${bold}service template${reset} using ${emacs_path@Q}"
+
+  if ! sed "s_${DEFAULT_EMACS_PATH}_${emacs_path}_" "$DIR/templates/emacs.service" > "$TARGET_PATH/emacs.service"; then
+    die
+  fi
+  live
+}
+
+function wal::enable_service() {
+  signal "Enabling service"
+
+  if ! systemctl --user enable emacs &> "$WAL_LOG"; then
+    die
+  fi
+  live
+}
+
+function main() {
+  cd ..
+
+  echo -e "${blue}${whale}${reset} ${bold}[daemon setup]${reset}"
+
+  wal::copy_service_template
+  wal::enable_service
+
+  echo -e "${green}${whale}${reset} daemon setup complete"
+}
+
+main
--- setup/wal-setup.el
+++ setup/wal-setup.el
@@ -2,70 +2,98 @@
 
 ;;; Commentary:
 
-;; Load this file using `load-file' and pick a setup.
-;; hooks.
+;; Load this file using `load-file', then run `wal/setup'.
 
 ;;; Code:
 
-(require 'shell nil t)
+(require 'shell)
+(require 'find-func)
 
-;; Internal
+;;;; Variables:
 
-(defvar wal/setup--results-buffer-name "*wal-results*"
-  "The name of the results buffer.")
+(defvar wal/setup--buffer-name "*wal-results*")
+(defvar wal/setup--dependencies '("bash"))
+(defvar wal/setup--optional-dependencies '("npm" "npx"))
+(defvar wal/setup--command-fmt "./%s.sh %s")
+(defvar wal/setup--scripts '(("init file" . wal/setup-init-file)
+                             ("commit hooks" . wal/setup-commit-hooks)
+                             ("daemon" . wal/setup-daemon)))
 
-(defun wal/setup--get-config-path ()
-  "Get the path to the config."
-  (expand-file-name ".." (file-name-directory (or load-file-name buffer-file-name))))
+;;;; Utility:
 
-(defun wal/setup--show-results-buffer ()
-  "Show the results buffer.
-Optionally ERASE-FIRST."
-  (with-current-buffer (get-buffer-create "*wal-results*")
-    (erase-buffer)
-    (display-buffer-in-side-window (current-buffer) '((side . bottom)))))
+(defun wal/setup--locate-config-root()
+  "Locate the config root."
+  (locate-dominating-file (find-library-name "wal-setup") "README.org"))
+
+(defun wal/setup--relative-file (file)
+  "Locate FILE relative to setup root."
+  (expand-file-name file (wal/setup--locate-config-root)))
 
 (defun wal/setup--executables-exist-p (&optional optional)
   "Check if (OPTIONAL) executables needed for setting up commits exist."
-  (let ((execs (if optional '("bash" "npm" "npx") '("bash"))))
+  (let ((execs (if optional
+                   (append wal/setup--optional-dependencies wal/setup--dependencies)
+                 wal/setup--dependencies)))
     (eval `(and ,@(mapcar #'executable-find execs)))))
 
+(defun wal/setup--get-buffer ()
+  "Get the results buffer."
+  (get-buffer-create wal/setup--buffer-name))
+
+(defun wal/setup--show-results ()
+  "Show the results buffer."
+  (with-current-buffer (wal/setup--get-buffer)
+    (erase-buffer)
+    (display-buffer-in-side-window (current-buffer) '((side . bottom)))))
+
+(defun wal/setup--command (name args)
+  "Return the command for the script NAME with ARGS."
+  (format wal/setup--command-fmt name (string-join args " ")))
+
 (defun wal/setup--async-run-script (script &optional args)
-  "Run SCRIPT asynchronously using results buffer as output (and ARGS)."
-  (let ((default-directory (expand-file-name "setup" (wal/setup--get-config-path)))
-        (scr (string-trim (concat "./" script ".sh" " " (string-join args " ")))))
-    (async-shell-command scr (get-buffer-create wal/setup--results-buffer-name))))
+  "Run SCRIPT, passing ARGS, asynchronously."
+  (let ((default-directory (wal/setup--relative-file "setup")))
+    (async-shell-command
+     (wal/setup--command script args)
+     (wal/setup--get-buffer))))
 
-;; Setup functions
+;; Setup functions:
 
-(defun wal/setup-init-file ()
-  "Set up the init file."
+(defun wal/setup-init-file (method)
+  "Set up the init file using METHOD."
+  (interactive (list (completing-read "Select setup method: "
+                                      '(link copy))))
   (unless (wal/setup--executables-exist-p)
     (user-error "Setup script requires bash to be in path"))
-  (let ((method (completing-read "Select setup method: " '(link copy))))
-    (wal/setup--show-results-buffer)
-    (wal/setup--async-run-script "setup-init" (list method user-emacs-directory))))
+  (wal/setup--show-results)
+  (wal/setup--async-run-script "setup-init" (list method user-emacs-directory)))
 
 (defun wal/setup-commit-hooks ()
   "Set up commit hooks."
+  (interactive)
   (unless (wal/setup--executables-exist-p t)
     (user-error "Setup script requires bash, npm and npx to be in path"))
-  (wal/setup--show-results-buffer)
+  (wal/setup--show-results)
   (wal/setup--async-run-script "setup-commits"))
 
-(defun wal/setup ()
-  "Run a setup function."
-  (let ((setup-name (completing-read
-                     "What do you want to set up? "
-                     '("init file" "commit hooks"))))
-    (cond
-     ((string= setup-name "init file")
-      (wal/setup-init-file))
-     ((string= setup-name "commit hooks")
-      (wal/setup-commit-hooks))
-     (t
-      (user-error "No setup of that name exists")))))
+(defun wal/setup-daemon ()
+  "Setup Emacs daemon."
+  (interactive)
+  (unless (wal/setup--executables-exist-p)
+    (user-error "Setup script requires bash to be in path"))
+  (wal/setup--show-results)
+  (wal/setup--async-run-script "setup-daemon"))
 
-(wal/setup)
+(defun wal/setup (script)
+  "Run a setup SCRIPT."
+  (interactive (list (completing-read
+                      "What do you want to set up? "
+                      wal/setup--scripts)))
+  (let ((func (cdr (assoc script wal/setup--scripts))))
+    (if func
+        (funcall func)
+      (user-error "No setup of that name exists"))))
+
+(provide 'wal-setup)
 
 ;;; wal-setup.el ends here
