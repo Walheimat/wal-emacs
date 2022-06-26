d335a2e665925397c360c130c74d5a2630bb8c0a
Walheimat <krister.schuchardt@gmail.com>
2022-06-26 20:23:03 +0200

    chore(commitlint): lint config with prettier

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,17 +3,17 @@ module.exports = {
 		'header-max-length': [2, 'always', 72], // GitHub's limit, GitLab's is 1 KiB
 		'type-empty': [2, 'never'],
 		'type-case': [1, 'always', ['lower-case']],
-        'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
-        'scope-empty': [1, 'never'],
+		'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
+		'scope-empty': [1, 'never'],
 		'subject-empty': [2, 'never'],
 		'subject-case': [2, 'always', ['lower-case']],
-        'subject-full-stop': [2, 'never'],
+		'subject-full-stop': [2, 'never'],
 		'body-empty': [1, 'never'],
-        'body-leading-blank': [2, 'always'],
+		'body-leading-blank': [2, 'always'],
 		'body-case': [2, 'always', ['sentence-case']],
 		'body-full-stop': [2, 'always', '.'],
 		'body-max-line-length': [2, 'always', 70],
-		'footer-max-line-length': [2, 'always', 70]
+		'footer-max-line-length': [2, 'always', 70],
 	},
-    ignores: [(commit) => commit.includes('fixup')],
+	ignores: [(commit) => commit.includes('fixup')],
 };
