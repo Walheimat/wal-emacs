a10a300e9de3b8d717a344ba34e97ffae0d0e81c
Walheimat <krister.schuchardt@gmail.com>
2023-01-04 19:05:34 +0100

    improve(tests): complete coverage for visual tests

--- test/wal-visuals-test.el
+++ test/wal-visuals-test.el
@@ -9,18 +9,30 @@
 (require 'wal-visuals nil t)
 
 (ert-deftest test-wal/with-recent-files-excluded ()
-  (should (equal '(testing this) (wal/with-recent-files-excluded #'wal/ra 'testing 'this))))
+  (defvar recentf-exclude nil)
+
+  (let ((fun (lambda (_) recentf-exclude)))
+
+    (should (equal '("bookmarks\\'" "zettelkasten") (wal/with-recent-files-excluded fun 'test)))))
 
 (ert-deftest test-wal/instead-show-biased-random ()
   (defvar dashboard-footer-messages '("Testing"))
   (should (equal "Testing" (wal/instead-show-biased-random))))
 
 (ert-deftest test-wal/in-case-of-daemonp-add-different-hook ()
-  (with-mock ((daemonp . #'always) add-hook)
+  (with-mock (require (daemonp . #'always) add-hook)
 
     (wal/in-case-of-daemonp-add-different-hook)
 
-    (was-called-with add-hook (list 'server-after-make-frame-hook #'dashboard-insert-startupify-lists))))
+    (should (string-equal "*dashboard*" (buffer-name (funcall initial-buffer-choice))))
+    (was-called-with require (list 'all-the-icons nil t))
+    (was-called-with add-hook (list 'server-after-make-frame-hook #'dashboard-insert-startupify-lists)))
+
+  (with-mock ((daemonp . #'ignore) add-hook)
+
+    (wal/in-case-of-daemonp-add-different-hook)
+
+    (was-not-called add-hook)))
 
 (ert-deftest test-wal/with-common-ligatures ()
   (defvar wal/common-ligatures)
