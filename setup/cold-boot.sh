f729b460dbb9fad83a0520786090e0548b63cc20
Walheimat <krister.schuchardt@gmail.com>
2023-01-22 13:16:06 +0100

    fix(ci): load actual script and use env

--- setup/cold-boot.sh
+++ setup/cold-boot.sh
@@ -10,4 +10,4 @@ cd "$SETUP_DIR/".. || exit 1
 EMACS_SOURCE_DIR="$(pwd)"
 export EMACS_SOURCE_DIR
 
-emacs -batch -l "$SETUP_DIR/wal-setup-cold-boot.el"
+emacs --batch -l "$SETUP_DIR/wal-setup-cold-boot.el"
