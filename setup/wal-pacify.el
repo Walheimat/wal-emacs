424d26f71173eb38538a95458cfa627c63efbcb7
Walheimat <krister.schuchardt@gmail.com>
2023-02-23 22:57:06 +0100

    chore(git, setup): add pre-push hook to flycheck packages

---
+++ setup/wal-pacify.el
@@ -0,0 +1,69 @@
+;;; wal-pacify.el --- Set up checking package files.
+
+;;; Commentary:
+
+;; This will just check the packages.
+
+;;; Code:
+
+(require 'wal-prelude (expand-file-name
+                       "setup/wal-prelude.el"
+                       (getenv "EMACS_SOURCE_DIR"))
+         t)
+
+(let ((source-dir (getenv "EMACS_SOURCE_DIR")))
+
+  (when (fboundp 'wal/bootstrap-config)
+    (wal/bootstrap-config source-dir t)))
+
+(defvar wal/emacs-config-package-path)
+(declare-function wal/package-files "ext:wal-prelude.el")
+
+(add-to-list 'load-path wal/emacs-config-package-path)
+(require 'wal nil t)
+
+(package-initialize)
+(require 'flycheck)
+
+;;; Functionality:
+
+(defvar wal-pacify-errors nil)
+
+(defun wal-pacify--callback ()
+  "Exit with errors."
+  (lambda (_status &optional errors)
+    (when errors
+      (seq-do (lambda (err)
+                (when-let ((err-message (flycheck-error-message err))
+                           (line (flycheck-error-line err))
+                           (file (flycheck-error-filename err)))
+
+                  (message "file: %s, line %d: %s\n" file line err-message)))
+              errors)
+      (setq wal-pacify-errors t))))
+
+(defun wal-pacify--check-file (file)
+  "Check FILE with flycheck."
+  (with-current-buffer (find-file-noselect file)
+    (let ((checker (flycheck-get-checker-for-buffer)))
+
+      (let ((check (flycheck-syntax-check-new
+                    :buffer (current-buffer)
+                    :checker checker
+                    :context nil
+                    :working-directory (flycheck-compute-working-directory checker)))
+            (callback (wal-pacify--callback)))
+
+        (flycheck-syntax-check-start check callback)))))
+
+(dolist (it (wal/package-files))
+  (wal-pacify--check-file it))
+
+(sit-for 5)
+
+(flycheck-safe-delete-temporaries)
+
+(when wal-pacify-errors
+  (kill-emacs 1))
+
+;;; wal-pacify.el ends here
