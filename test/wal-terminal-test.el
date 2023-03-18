0fa98a057c714b9f12ec7414d0fb0dca54b1035f
Walheimat <krister.schuchardt@gmail.com>
2023-03-18 21:16:07 +0100

    package(vterm): mothball

--- test/wal-terminal-test.el
+++ test/wal-terminal-test.el
@@ -8,52 +8,6 @@
 
 (require 'wal-terminal nil t)
 
-(ert-deftest test-wal/vterm-run--errors-for-unknown ()
-  (should-error (wal/vterm-run "asdf") :type 'user-error))
-
-(ert-deftest test-wal/vterm-run--displays-existing-buffer ()
-  (let ((buf (get-buffer-create "*vterm-run-test")))
-    (with-mock (display-buffer
-                (buffer-list . (lambda (&optional _) (list buf))))
-
-      (wal/vterm-run "test")
-
-      (was-called-with display-buffer (list buf nil)))))
-
-(ert-deftest test-wal/vterm-run--sends-command ()
-  (with-mock ((buffer-list . #'ignore)
-              vterm-mode
-              vterm-send-string
-              vterm-send-return
-              display-buffer)
-    (wal/vterm-run "test")
-
-    (let ((buf (get-buffer "*vterm-run-test*")))
-
-      (was-called-with display-buffer (list buf nil))
-      (was-called vterm-mode)
-      (was-called-with vterm-send-string "test")
-      (was-called vterm-send-return))))
-
-(ert-deftest test-wal/vterm--calls-vterm-outside-project ()
-  (with-mock ((project-current . #'ignore) vterm)
-
-    (wal/vterm)
-
-    (was-called vterm)))
-
-(ert-deftest test-wal/vterm--checks-project-buffers ()
-  (with-temp-buffer
-    (setq-local major-mode 'vterm-mode)
-    (rename-buffer "VTerm: test")
-    (with-mock ((project-current . #'always)
-                (project-buffers . (lambda (_) (list (current-buffer))))
-                switch-to-buffer)
-
-      (wal/vterm)
-
-      (was-called-with switch-to-buffer (list (current-buffer))))))
-
 (ert-deftest test-wal/instead-truncate-buffer ()
   (with-mock eshell-truncate-buffer
     (wal/instead-truncate-buffer)
