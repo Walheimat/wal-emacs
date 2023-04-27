3788ad0f1df04b003c1547cae902a2c1457a972e
Walheimat <krister.schuchardt@gmail.com>
2023-04-27 20:42:15 +0200

    improve(windows): wal/tab-bar-switch-to-buffer-tab selects window

--- test/wal-windows-test.el
+++ test/wal-windows-test.el
@@ -13,17 +13,20 @@
 
     (with-mock ((tab-bar-get-buffer-tab . (lambda (_) found))
                 tab-bar-switch-to-tab
-                switch-to-buffer)
+                switch-to-buffer
+                select-window
+                get-buffer-window)
 
       (wal/tab-bar-switch-to-buffer-tab 'buffer)
 
       (was-called-with switch-to-buffer (list 'buffer))
-
       (wal/clear-mocks)
       (setq found '((name . "test-tab")))
 
       (wal/tab-bar-switch-to-buffer-tab 'buffer)
 
-      (was-called-with tab-bar-switch-to-tab "test-tab"))))
+      (was-called-with tab-bar-switch-to-tab "test-tab")
+      (was-called-with get-buffer-window (list 'buffer))
+      (was-called select-window))))
 
 ;;; wal-windows-test.el ends here
