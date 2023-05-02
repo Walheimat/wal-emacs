3b260d2958dd5f67c4e37b49401a0407f58a5e37
Walheimat <krister.schuchardt@gmail.com>
2023-05-02 22:04:52 +0200

    fix: trash outdated dir-locals template

--- templates/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; dir local variables to work with space indentation
-
-((js2-mode  . ((wal/prefer-tabs . nil)
-			   (js-indent-level . 2)))
- (css-mode  . ((wal/prefer-tabs   . nil)
-			   (css-indent-offset . 2))))
