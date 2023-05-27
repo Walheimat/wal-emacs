66c3d74a6280af7540fcc283797b6d3dd3932c1b
Walheimat <krister.schuchardt@gmail.com>
2023-05-27 13:06:10 +0200

    refac(windows): group partial-recall functions

--- test/wal-windows-test.el
+++ test/wal-windows-test.el
@@ -61,40 +61,40 @@
        (with-temp-buffer
          ,@body))))
 
-(ert-deftest wpr--remember--remembers ()
+(ert-deftest wpr-remember--remembers ()
   (with-tab-history
 
-   (wal-partial-recall--remember)
+   (wal-partial-recall-remember)
 
    (should-not (null (gethash (alist-get 'wpr test-tab) wal-partial-recall--table)))))
 
-(ert-deftest wpr--remember--inserts-once ()
+(ert-deftest wpr-remember--inserts-once ()
   (defvar wal-partial-recall--table)
 
   (with-tab-history
 
-    (wal-partial-recall--remember)
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
+    (wal-partial-recall-remember)
 
     (let* ((memory (gethash (alist-get 'wpr test-tab) wal-partial-recall--table))
            (ring (wal-partial-recall--memory-ring memory)))
 
       (should (eq 1 (ring-length ring))))))
 
-(ert-deftest wpr--remember--extends-ring ()
+(ert-deftest wpr-remember--extends-ring ()
   (with-tab-history
     (with-mock ((wal-partial-recall--at-capacity . #'always)
                 (wal-partial-recall--should-extend-p . #'always)
                 ring-extend)
 
-      (wal-partial-recall--remember (current-buffer))
+      (wal-partial-recall-remember (current-buffer))
       (was-called ring-extend))))
 
-(ert-deftest wpr--forget--forgets ()
+(ert-deftest wpr-forget--forgets ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
-    (wal-partial-recall--forget)
+    (wal-partial-recall-forget)
 
     (let* ((memory (gethash (alist-get 'wpr test-tab) wal-partial-recall--table))
            (ring (wal-partial-recall--memory-ring memory)))
@@ -103,7 +103,7 @@
 
 (ert-deftest wpr--on-close ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (should (eq 1 (length (hash-table-keys wal-partial-recall--table))))
 
@@ -119,32 +119,32 @@
   (with-tab-history
     (should-not (wal-partial-recall--moments))
 
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (should (wal-partial-recall--moments))))
 
 (ert-deftest wpr--current-p ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
     (should (wal-partial-recall--current-p (current-buffer)))))
 
 (ert-deftest wpr--has-buffers-p ()
   (with-tab-history
     (should-not (wal-partial-recall--has-buffers-p))
 
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (should (wal-partial-recall--has-buffers-p))))
 
 (ert-deftest wpr--known-buffer-p ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (should (wal-partial-recall--known-buffer-p (current-buffer)))))
 
 (ert-deftest wpr--memory-buffer-p ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (let ((memory (gethash (alist-get 'wpr test-tab) wal-partial-recall--table)))
 
@@ -152,7 +152,7 @@
 
 (ert-deftest wpr-moment-buffer-p ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (let* ((memory (gethash (alist-get 'wpr test-tab) wal-partial-recall--table))
            (ring (wal-partial-recall--memory-ring memory))
@@ -162,46 +162,47 @@
 
 (ert-deftest wpr--maybe-remember ()
   (with-tab-history
-    (with-mock (wal-partial-recall--remember
+    (with-mock (wal-partial-recall-remember
                 (buffer-live-p . #'always)
                 (wal-partial-recall--known-buffer-p . #'ignore))
 
       (wal-partial-recall--maybe-remember (current-buffer))
-      (was-called wal-partial-recall--remember))))
+      (was-called wal-partial-recall-remember))))
 
 (ert-deftest wpr--maybe-remember--reclaims ()
   (with-tab-history
-    (with-mock (wal-partial-recall--remember
-                wal-partial-recall--maybe-reclaim
+    (with-mock (wal-partial-recall-remember
+                wal-partial-recall-reclaim
                 (buffer-live-p . #'always)
                 (wal-partial-recall--known-buffer-p . #'always))
 
       (wal-partial-recall--maybe-remember (current-buffer))
-      (was-called wal-partial-recall--maybe-reclaim))))
+      (was-called wal-partial-recall-reclaim))))
 
-(ert-deftest wpr--maybe-reclaim--reclaims-from-other ()
+(ert-deftest wpr-reclaim--reclaims-from-other ()
   (let ((seconds '(10 12))
         (wal-partial-recall-reclaim-threshold 0))
     (with-tab-history
       (with-mock ((time-to-seconds . (lambda (&rest _) (pop seconds))))
-        (wal-partial-recall--remember)
+        (wal-partial-recall-remember)
 
-        (with-mock ((wal-partial-recall-current . (lambda () 'other))
-                    wal-partial-recall--remember)
+        (with-mock ((wal-partial-recall--current . (lambda () 'other))
+                    wal-partial-recall-remember)
 
-          (wal-partial-recall--maybe-reclaim (current-buffer))
+          (wal-partial-recall-reclaim (current-buffer))
 
-          (was-called wal-partial-recall--remember))))))
+          (was-called wal-partial-recall-remember))))))
 
-(ert-deftest wpr--maybe-reclaim--no-op-for-same ()
+(ert-deftest wpr-reclaim--no-op-for-same ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
-    (with-mock (wal-partial-recall--remember)
+    (with-mock (wal-partial-recall-remember)
 
-        (wal-partial-recall--maybe-reclaim (current-buffer))
+      (with-current-buffer (current-buffer)
+        (wal-partial-recall-reclaim))
 
-        (was-not-called wal-partial-recall--remember))))
+      (was-not-called wal-partial-recall-remember))))
 
 (ert-deftest wpr--should-extend-p ()
   (let ((seconds '(10 11 12))
@@ -211,20 +212,20 @@
     (with-tab-history
       (with-mock ((time-to-seconds . (lambda (&rest _) (pop seconds))))
 
-        (wal-partial-recall--remember)
+        (wal-partial-recall-remember)
 
         (let ((memory (wal-partial-recall--get-or-create-memory (wal-partial-recall--key))))
 
           (should (wal-partial-recall--should-extend-p memory))
           (should-not (wal-partial-recall--should-extend-p memory)))))))
 
-(ert-deftest wpr-recall-owner ()
+(ert-deftest wpr-recall--owner ()
   (with-tab-history
-    (wal-partial-recall--remember)
+    (wal-partial-recall-remember)
 
     (let ((memory (wal-partial-recall--get-or-create-memory (wal-partial-recall--key))))
 
-      (should (eq memory (wal-partial-recall-owner))))))
+      (should (eq memory (wal-partial-recall--owner))))))
 
 (ert-deftest wpr--on-buffer-list-update--cancels-running-timer ()
   (let ((wal-partial-recall--timer nil))
