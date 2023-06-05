3bf5bf76aa4a7f7e3c96de166b3af98ff7f447dc
Walheimat <krister.schuchardt@gmail.com>
2023-06-05 22:24:00 +0200

    config(commitlint): update rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,9 +2,13 @@ module.exports = {
 	rules: {
 		'header-max-length': [2, 'always', 72], // GitHub's limit, GitLab's is 1 KiB
 		'type-empty': [2, 'never'],
-		'type-case': [1, 'always', ['lower-case']],
+		'type-enum': [
+			2,
+			'always',
+			['chore', 'ci', 'config', 'docs', 'feat', 'fix', 'improve', 'refac', 'style', 'test'],
+		],
 		'scope-case': [2, 'always', ['lower-case', 'kebab-case']],
-		'scope-empty': [1, 'never'],
+		'scope-empty': [2, 'never'],
 		'subject-empty': [2, 'never'],
 		'subject-case': [1, 'always', ['lower-case']],
 		'subject-full-stop': [2, 'never'],
