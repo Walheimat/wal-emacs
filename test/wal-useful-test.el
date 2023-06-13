c6333c4757ea6b233c2b8e1312a099dd109023b1
Walheimat <krister.schuchardt@gmail.com>
2023-06-13 18:13:44 +0200

    fix(test): expanding parallel tests

--- test/wal-useful-test.el
+++ test/wal-useful-test.el
@@ -423,43 +423,46 @@
 (ert-deftest test-parallel ()
   (bydi-match-expansion
    (parallel some-fun other-fun)
-   `(defun some-fun||other-fun (&optional arg)
-      "Call `some-fun' or `other-fun' depending on prefix argument.\nNo argument means: call the prior. Numeric prefix `0' means: call the latter.\n\nFor all other prefix values: numeric prefixes call the latter,\n`universal-argument' prefixes call the prior."
-      (interactive "P")
-
-      (cond
-       ((not arg)
-        (call-interactively ',some-fun))
-       ((equal 0 arg)
-        (setq current-prefix-arg nil)
-        (prefix-command-update)
-        (call-interactively ',other-fun))
-       ((equal (prefix-numeric-value arg) arg)
-        (call-interactively ',other-fun))
-       (t
-        (call-interactively 'some-fun))))))
+   '(defun some-fun||other-fun (&optional arg)
+     "Call `some-fun' or `other-fun' depending on prefix argument.\nNo argument means: call the prior. Numeric prefix `0' means: call the latter.\n\nFor all other prefix values: numeric prefixes call the latter,\n`universal-argument' prefixes call the prior."
+     (interactive "P")
+     (cond
+      ((not arg)
+       (call-interactively 'some-fun))
+      ((equal 0 arg)
+       (setq current-prefix-arg nil)
+       (prefix-command-update)
+       (call-interactively 'other-fun))
+      ((equal
+        (prefix-numeric-value arg)
+        arg)
+       (call-interactively 'other-fun))
+      (t
+       (call-interactively 'some-fun))))))
 
 (ert-deftest test-parallel--universalize ()
   (bydi-match-expansion
    (parallel some-fun other-fun :universalize t)
-   `(defun some-fun||other-fun (&optional arg)
-      "Call `some-fun' or `other-fun' depending on prefix argument.\nNo argument means: call the prior. Numeric prefix `0' means: call the latter.\n\nFor all other prefix values: numeric prefixes call the latter,\n`universal-argument' prefixes call the prior.\n\nThis function is universalized."
-      (interactive "P")
-
-      (cond
-       ((not arg)
-        (call-interactively ',some-fun))
-       ((equal 0 arg)
-        (setq current-prefix-arg nil)
-        (prefix-command-update)
-        (call-interactively ',other-fun))
-       ((equal (prefix-numeric-value arg) arg)
-        (progn
-          (setq current-prefix-arg (list arg))
-          (prefix-command-update)
-          (call-interactively ',other-fun)))
-       (t
-        (call-interactively 'some-fun))))))
+   '(defun some-fun||other-fun (&optional arg)
+     "Call `some-fun' or `other-fun' depending on prefix argument.\nNo argument means: call the prior. Numeric prefix `0' means: call the latter.\n\nFor all other prefix values: numeric prefixes call the latter,\n`universal-argument' prefixes call the prior.\n\nThis function is universalized."
+     (interactive "P")
+     (cond
+      ((not arg)
+       (call-interactively 'some-fun))
+      ((equal 0 arg)
+       (setq current-prefix-arg nil)
+       (prefix-command-update)
+       (call-interactively 'other-fun))
+      ((equal
+        (prefix-numeric-value arg)
+        arg)
+       (progn
+         (setq current-prefix-arg
+               (list arg))
+         (prefix-command-update)
+         (call-interactively 'other-fun)))
+      (t
+       (call-interactively 'some-fun))))))
 
 (ert-deftest test-wal-scratch-buffer ()
   (bydi ((:mock pop-to-buffer :with (lambda (n &rest _) (buffer-name n))))
