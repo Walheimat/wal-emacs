d252185249ca22fb58ca704985d85e9e225e603d
Walheimat <krister.schuchardt@gmail.com>
2024-03-06 18:05:36 +0100

    config(ace-window): remove again

--- test/wal-windows-test.el
+++ test/wal-windows-test.el
@@ -8,21 +8,6 @@
 
 (require 'wal-windows nil t)
 
-(ert-deftest wal-in-case-of-single-window ()
-  :tags '(windows)
-
-  (let ((windows nil))
-    (bydi ((:mock wal-interesting-windows :return windows)
-           wal-switch-to-other-buffer)
-
-      (should-not (wal-in-case-of-single-window-switch-to-other-buffer))
-
-      (setq windows '(a b))
-
-      (wal-in-case-of-single-window-switch-to-other-buffer)
-
-      (bydi-was-called wal-switch-to-other-buffer))))
-
 ;;; wal-windows-test.el ends here
 
 ;; Local Variables:
