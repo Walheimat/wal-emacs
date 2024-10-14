311cdc17ace979a1a6c95c4488a0433cf8f665cb
Walheimat <krister.schuchardt@gmail.com>
2024-10-14 21:41:04 +0200

    improve(jinx): add predicate to exclude JavaScript exports
    The predicate is only added for JS modes.

--- test/wal-fix-test.el
+++ test/wal-fix-test.el
@@ -8,6 +8,22 @@
 
 (require 'wal-fix nil t)
 
+(defvar js-imports (ert-resource-file "js.txt"))
+
+(ert-deftest wal-jinx--js-import-p ()
+  :tags '(fix)
+
+  (ert-with-test-buffer (:name "jinx-js")
+    (insert-file-contents js-imports)
+    (goto-char 0)
+    (should (wal-jinx--js-import-p (point)))
+    (next-line)
+    (should-not (wal-jinx--js-import-p (point)))
+    (next-line)
+    (should-not (wal-jinx--js-import-p (point)))
+    (next-line 3)
+    (should (wal-jinx--js-import-p (point)))))
+
 ;;; wal-fix-test.el ends here
 
 ;; Local Variables:
