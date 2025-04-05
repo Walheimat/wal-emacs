974cb4cf5b4eee467a7f38dcdb1378bd9e294554
Walheimat <krister.schuchardt@gmail.com>
2025-04-05 15:03:19 +0200

    improve(org-agenda): remove renaming altogether
    The newest change introduced a bug that would delete any other tab and
    then make closing the agenda impossible.

--- test/wal-org-test.el
+++ test/wal-org-test.el
@@ -268,34 +268,6 @@
 
       (should (string= "Testing" (buffer-string))))))
 
-(ert-deftest wal-org-agenda--then-rename-tab ()
-  :tags '(org)
-
-  (defvar org-agenda-window-setup nil)
-
-  (let ((org-agenda-window-setup 'something-else))
-
-    (bydi (tab-bar-rename-tab
-           tab-bar-close-tab
-           (:mock tab-bar--tab-index-by-name :var index))
-
-      (wal-org-agenda--then-rename-tab)
-
-      (bydi-was-not-called tab-bar-rename-tab)
-
-      (setq org-agenda-window-setup 'other-tab)
-
-      (wal-org-agenda--then-rename-tab)
-
-      (bydi-was-not-called tab-bar-close-tab)
-      (bydi-was-called-with tab-bar-rename-tab "agenda")
-
-      (setq index 1)
-
-      (wal-org-agenda--then-rename-tab)
-
-      (bydi-was-called-with tab-bar-close-tab 2))))
-
 (ert-deftest wal-org-super-agenda--with-groups ()
   :tags '(org)
 
